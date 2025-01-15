16d01d8888ccc0efe2101e0c122b903e4b6e4941
Emanuel M <emanuel.munera1@gmail.com>
2025-01-14 19:17:25 -0500

    Began implementation of methods and functions to interact with the tasks and the task list

--- structs/structs.go
+++ structs/structs.go
@@ -1,6 +1,7 @@
 package structs
 
 import (
+	"iter"
 	"task-tracker-cli/enums"
 	"time"
 )
@@ -13,6 +14,42 @@ type Task struct {
 	updatedAt   time.Time
 }
 
+func NewTask(id int, description string, status enums.TaskState, createdAT, updatedAt time.Time) *Task {
+	return &Task{
+		id,
+		description,
+		status,
+		createdAT,
+		updatedAt,
+	}
+}
+
 type TaskList struct {
-	tasks []Task
+	lastAddedId int
+	tasks       []Task
+}
+
+func NewTaskList() *TaskList {
+	return &TaskList{
+		1,
+		make([]Task, 0, 10),
+	}
+}
+
+func (tl *TaskList) AddTask(task Task) {
+	tl.tasks = append(tl.tasks, task)
+}
+
+func (tl *TaskList) GetLastAddedId() int {
+	return tl.lastAddedId
+}
+
+func (tl *TaskList) All() iter.Seq[Task] {
+	return func(yield func(Task) bool) {
+		for i := 0; i < len(tl.tasks); i++ {
+			if !yield(tl.tasks[i]) {
+				return
+			}
+		}
+	}
 }
