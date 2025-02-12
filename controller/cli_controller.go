a29c7e630b99866356aaadd88b52ab2d683785bc
Emanuel M <emanuel.munera1@gmail.com>
2025-02-12 14:12:58 -0500

    Implemented logic to switch statement to filter actions by argument

--- controller/cli_controller.go
+++ controller/cli_controller.go
@@ -2,21 +2,72 @@ package controller
 
 import (
 	"fmt"
+	"strconv"
+	"task-tracker-cli/enums"
 )
 
 func ProcessArgs(args []string) {
 	switch args[1] {
 	case "add":
-		fmt.Print("adding")
+		argument := ""
+		if len(args) >= 3 {
+			argument = args[2]
+		}
+		addTask(argument)
+
 	case "list":
-		fmt.Print("listing")
+		argument := ""
+		if len(args) >= 3 {
+			argument = args[2]
+		}
+		listTasks(argument)
+
 	case "delete":
-		fmt.Print("deleting")
+		argument := 0
+		if len(args) >= 3 {
+			argument, _ = strconv.Atoi(args[2])
+		}
+		deleteTask(argument)
+
 	case "update":
-		fmt.Print("updating")
+		if len(args) >= 4 {
+			arg1, _ := strconv.Atoi(args[2])
+			updateTask(arg1, args[3])
+		} else {
+			panic("Not enough arguments")
+		}
+
 	case "mark-in-progress":
-		fmt.Print("marking in progress")
+		if len(args) >= 3 {
+			argument, _ := strconv.Atoi(args[2])
+			updateStatus(argument, enums.InProgress)
+		}
+
 	case "mark-done":
-		fmt.Print("marking done")
+		if len(args) >= 3 {
+			argument, _ := strconv.Atoi(args[2])
+			updateStatus(argument, enums.Done)
+		}
 	}
+}
+
+func addTask(desc string) {
+	fmt.Print("consuming service to add a task: " + desc)
+}
+
+func listTasks(status string) {
+	fmt.Print("listing tasks: " + status)
+}
+
+func deleteTask(id int) {
+	fmt.Print("deleting task: " + string(id))
+}
+
+func updateTask(id int, desc string) {
+	fmt.Print("Updating task " + string(id) + " with " + desc)
+}
+
+func updateStatus(id int, status enums.TaskState) {
+	fmt.Print("Changing status of task " + string(id) + " to " + status.String())
+
 }
